9a68c689ac10c9f090ba41d630e87e9f3e6490fd
slav4ocom <slav4o_1@abv.bg>
2021-03-20 01:28:36 +0300

    Enhanced class structure. Added Commands.cs

--- Engine.cs
+++ Engine.cs
@@ -11,7 +11,7 @@ namespace Console_Manager
 {
     public static class Engine
     {
-        static ElectronicsWarehouseContext db;
+        public static ElectronicsWarehouseContext db { get; private set; }
         public static void Run()
         {
             db = new ElectronicsWarehouseContext();
@@ -46,54 +46,19 @@ namespace Console_Manager
                     break;
 
                 case "add":
-                    Console.WriteLine("Add new part: name, part type, price ");
-                    var arguments = Console.ReadLine()
-                        .Split(",", StringSplitOptions.RemoveEmptyEntries)
-                        .Select(a => a.Trim())
-                        .ToArray();
-
-                    db.Add(new Article()
-                    {
-                        Name = arguments[0],
-                        PartType = arguments[1],
-                        Price = decimal.Parse(arguments[2])
-                    });
-                    db.SaveChanges();
+                    Commands.Add();
                     break;
 
                 case "list":
-                    db.Articles.ToList().ForEach(a => Console.WriteLine($"{a.Id}. {a.Name} {a.PartType} {a.Price} BGN"));
+                    Commands.List();
                     break;
 
                 case "remove":
-                    Console.WriteLine("Enter Id to remove:");
-                    var id = int.Parse(Console.ReadLine());
-                    db.Remove(db.Articles.FirstOrDefault(a => a.Id == id));
-                    db.SaveChanges();
+                    Commands.Remove();
                     break;
 
                 case "edit":
-                    Console.WriteLine("Enter Id to edit:");
-                    var idToEdit = int.Parse(Console.ReadLine());
-                    var part = db.Articles.FirstOrDefault(p => p.Id == idToEdit);
-                    if (part != null)
-                    {
-                        Console.WriteLine($"{part.Id}. {part.Name} {part.PartType} {part.Price} BGN");
-                        Console.WriteLine("Enter new data: name, part type, price ");
-                        var editArgs = Console.ReadLine()
-                            .Split(",", StringSplitOptions.RemoveEmptyEntries)
-                            .Select(a => a.Trim())
-                            .ToArray();
-
-                        part.Name = editArgs[0];
-                        part.PartType = editArgs[1];
-                        part.Price = decimal.Parse(editArgs[2]);
-                        db.SaveChanges();
-                    }
-                    else
-                    {
-                        Console.WriteLine("Invalid part.");
-                    }
+                    Commands.Edit();
                     break;
 
                 default:
