4dd4f080e3333fc4041f3e5477a72cc0a7dc5419
slav4ocom <slav4o_1@abv.bg>
2021-03-19 22:01:35 +0300

    Add Migrations

---
+++ Models/Article.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Console_Manager.Models
+{
+    public class Article
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+        public string PartType { get; set; }
+
+    }
+}
